e8f706d956922017f0331c39bd400a688eda12c2
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-06-03 18:13:20 +0100

    Created a function that allows the game names still available to be displayed in a dropdown select menu

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,8 +1,36 @@
 queue()
-    .defer(d3.csv, "data/ps4gamesales.csv")
+    .defer(d3.csv, "data/gamesalesData.csv")
     .await(makeGraphs);
     
-function makeGraphs(error, ps4gamesalesData) {
+function makeGraphs(error, gamesalesData) {
+    var ndx = crossfilter(gamesalesData)
     // TODO: make graphs using d3
+    gamesalesData.forEach(function(d){
+        d.north_america = parseInt(d["north_america"]);
+        d.europe = parseInt(d["europe"]);
+        d.rest_of_world = parseInt(d["rest_of_world"]);
+        d.global = parseInt(d["global"]);
+    });
 
+    /* ----- Video Game Selector -----*/
+    game_selector(ndx);
+
+    dc.renderAll();
 }
+
+
+
+
+/* ----- Video Game Selector Function -----*/
+
+function game_selector(ndx) {
+    var gameDim = ndx.dimension(dc.pluck("game"));
+    var gameGroup = gameDim.group();
+
+    dc.selectMenu("#game-selector")
+        .dimension(gameDim)
+        .group(gameGroup);
+        /*.title(function(d) {
+            return d.key;
+        });*/
+};
