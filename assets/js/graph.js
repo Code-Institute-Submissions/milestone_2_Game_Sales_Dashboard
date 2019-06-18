6d7a795bf56c1b40d0533d822843299dc89796ea
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2019-06-18 15:54:08 +0100

    made final touches to the graphs, removed the widths from the bar charts and allowed for them to be assigned widths through css rules, have also changed the widths of the pie charts so that they will fit comfortably and not extend the screen past its current viewpoint

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -3,17 +3,7 @@ queue()
     .await(makeGraphs);
     
 function makeGraphs(error, gamesalesData) {
-    var ndx = crossfilter(gamesalesData);
-
-    /*
-    gamesalesData.forEach(function(d){
-        d.north_america = parseFloat(d["north_america"]).toFixed(2);
-        d.europe = parseFloat(d["europe"]).toFixed(2);
-        d.rest_of_world = parseFloat(d["rest_of_world"]).toFixed(2);
-        d.global = parseFloat(d["global"]).toFixed(2);
-    });
-*/
-    //TODO  try and get the numbers data converted to 2 decimal places...
+    let ndx = crossfilter(gamesalesData);
 
     /* ----- Video Game Selector -----*/
     game_selector(ndx);
@@ -46,10 +36,11 @@ function makeGraphs(error, gamesalesData) {
 /*----- Video Game Selector Function -----*/
 
 function game_selector(ndx) {
-    var game_selector_dim = ndx.dimension(dc.pluck("game"));
-    var game_selector_group = game_selector_dim.group();
+    let game_selector_dim = ndx.dimension(dc.pluck("game"));
+    let game_selector_group = game_selector_dim.group();
 
     dc.selectMenu("#game-selector")
+        .useViewBoxResizing(true)
         .dimension(game_selector_dim)
         .group(game_selector_group);
 }
@@ -57,10 +48,11 @@ function game_selector(ndx) {
 /*----- Console Selector -----*/
 
 function console_selector(ndx) {
-    var console_selector_dim = ndx.dimension(dc.pluck("console"));
-    var console_selector_group = console_selector_dim.group();
+    let console_selector_dim = ndx.dimension(dc.pluck("console"));
+    let console_selector_group = console_selector_dim.group();
 
     dc.selectMenu("#console-selector")
+        .useViewBoxResizing(true)
         .dimension(console_selector_dim)
         .group(console_selector_group);
 }
@@ -68,10 +60,11 @@ function console_selector(ndx) {
 /*----- Genre Selector Function -----*/
 
 function genre_selector(ndx) {
-    var genre_selector_dim = ndx.dimension(dc.pluck("genre"));
-    var genre_selector_group = genre_selector_dim.group();
+    let genre_selector_dim = ndx.dimension(dc.pluck("genre"));
+    let genre_selector_group = genre_selector_dim.group();
 
     dc.selectMenu("#genre-selector")
+        .useViewBoxResizing(true)
         .dimension(genre_selector_dim)
         .group(genre_selector_group);
 }
@@ -79,10 +72,11 @@ function genre_selector(ndx) {
 /*----- Publisher Selector Function -----*/
 
 function publisher_selector(ndx) {
-    var publisher_selector_dim = ndx.dimension(dc.pluck("publisher"));
-    var publisher_selector_group = publisher_selector_dim.group();
+    let publisher_selector_dim = ndx.dimension(dc.pluck("publisher"));
+    let publisher_selector_group = publisher_selector_dim.group();
 
     dc.selectMenu("#publisher-selector")
+        .useViewBoxResizing(true)
         .dimension(publisher_selector_dim)
         .group(publisher_selector_group);
 }
@@ -90,35 +84,47 @@ function publisher_selector(ndx) {
 
 /* ----- Units Sold Per Console -----*/
 function console_game_sales_comparison(ndx) {
-    var console_game_sales_comparison_dim = ndx.dimension(dc.pluck("console"));
-    var console_game_sales_comparison_group = console_game_sales_comparison_dim.group().reduceSum(dc.pluck("global"));
+    let console_game_sales_comparison_dim = ndx.dimension(dc.pluck("console"));
+    let console_game_sales_comparison_group = console_game_sales_comparison_dim.group().reduceSum(dc.pluck("global"));
+
+    function console_game_sales_comparison_title (k, v) {
+        // Shows the console and sales on hover
+        if (k) {
+            return k + " sold " + three_decimal_places(v) + " million games.";
+        }
+    }
 
     dc.pieChart("#games-sold-comparison")
-        .height(300)
-        .width(550)
-        .radius(100)
-        .cx(200)
+        .height(250)
+        .radius(80)
+        .cx(80)
         .transitionDuration(1500)
-        .renderLabel(false)
+        .renderLabel(true)
+        .title(function(d) {
+            return console_game_sales_comparison_title(d.key, d.value);
+        })
         .dimension(console_game_sales_comparison_dim)
         .group(console_game_sales_comparison_group)
         .ordinalColors(['#003791', '#107c10'])
-        .legend(dc.legend().x(300).y(25).gap(3));
-        // TODO LOOK OVER THE ABOVE CODE TO TRY AND GET INDIVIDUAL TITLE WORKING
-        // TODO LOOK OVER THE ABOVE CODE TO TRY AND GET INDIVIDUAL TITLE WORKING
 }
 
 /* ----- Total Games Sold Per Genre Globally -----*/
  function global_genre_sales(ndx) {
-     var global_genre_sales_dimension = ndx.dimension(dc.pluck("genre"));
-     var global_genre_sales_group = global_genre_sales_dimension.group().reduceSum(dc.pluck("global"));
-     var chart_colors = d3.scale.ordinal()
+     let global_genre_sales_dimension = ndx.dimension(dc.pluck("genre"));
+     let global_genre_sales_group = global_genre_sales_dimension.group().reduceSum(dc.pluck("global"));
+     let chart_colors = d3.scale.ordinal()
          .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#ffffff', '#9800ff', '#ffa500']);
 
+     function global_genre_sales_title (k, v) {
+         // Shows the game sales per genre in North America on hover
+         if (k){
+             return "In the Top 100's, the " + k + " genre has sold " + three_decimal_places(v) + " million games Globally.";
+         }
+     }
+
      dc.barChart("#genre-global-sales")
-         .width(550)
-         .height(400)
-         .margins({top: 10, right: 50, bottom: 75, left: 40})
+         .height(450)
+         .margins({top: 10, right: 20, bottom: 75, left: 40})
          .dimension(global_genre_sales_dimension)
          .group(global_genre_sales_group)
          .transitionDelay(500)
@@ -126,9 +132,13 @@ function console_game_sales_comparison(ndx) {
              return d.key;
          })
          .colors(chart_colors)
+         .title(function(d) {
+             return global_genre_sales_title(d.key, d.value);
+         })
          .x(d3.scale.ordinal())
          .xUnits(dc.units.ordinal)
          .elasticY(true)
+         .useViewBoxResizing(true)
          .xAxisLabel("Genre")
          .yAxisLabel("Games sold (in millions)")
          .yAxis().ticks(8);
@@ -136,17 +146,25 @@ function console_game_sales_comparison(ndx) {
 
 /* ----- Total Games Sold Per Publisher -----*/
 function global_publisher_sales(ndx) {
-    var global_publisher_dimension = ndx.dimension(dc.pluck("publisher"));
-    var publisher_north_america_sales_group = global_publisher_dimension.group().reduceSum(dc.pluck("north_america"));
-    var publisher_europe_sales_group = global_publisher_dimension.group().reduceSum(dc.pluck("europe"));
-    var publisher_rest_of_world_sales_group = global_publisher_dimension.group().reduceSum(dc.pluck("rest_of_world"));
-    var chart_colors = d3.scale.ordinal()
+    let global_publisher_dimension = ndx.dimension(dc.pluck("publisher"));
+    let publisher_north_america_sales_group = global_publisher_dimension.group().reduceSum(dc.pluck("north_america"));
+    let publisher_europe_sales_group = global_publisher_dimension.group().reduceSum(dc.pluck("europe"));
+    let publisher_rest_of_world_sales_group = global_publisher_dimension.group().reduceSum(dc.pluck("rest_of_world"));
+    let chart_colors = d3.scale.ordinal()
         .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#f1f1f1', '#9800ff', '#ffa500']);
 
+    function global_publisher_sales_title (k, v) {
+        // Shows the Publisher and the number of games the publisher is accountable for on hover
+
+        if (k){
+            return "In the Top 100's, " + k + " have sold " + three_decimal_places(v) + " million games in..."
+        }
+        console.log(data);
+    }
+
     dc.barChart("#publisher-global-sales") //THIS BAR CHART WILL BE A STACKED BAR CHART
-        .width(750)
-        .height(400)
-        .margins({top: 10, right: 50, bottom: 105, left: 40})
+        .height(450)
+        .margins({top: 10, right: 20, bottom: 125, left: 40})
         .dimension(global_publisher_dimension)
         .group(publisher_north_america_sales_group, "North America")
         .stack(publisher_europe_sales_group, "Europe")
@@ -156,26 +174,35 @@ function global_publisher_sales(ndx) {
             return d.key;
         })
         .colors(chart_colors)
+        .title(function(d) {
+            return global_publisher_sales_title(d.key, d.value);
+        })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
+        .useViewBoxResizing(true)
         .xAxisLabel("Publisher")
         .yAxisLabel("Games sold (in millions)")
-        .yAxis().ticks(8)
+        .yAxis().ticks(8);
 }
 
 
 /* ----- Total Games Sold Per Genre in North America -----*/
 function north_america_genre_sales(ndx) {
-    var north_america_genre_dimension = ndx.dimension(dc.pluck("genre"));
-    var north_america_genre_sales_group = north_america_genre_dimension.group().reduceSum(dc.pluck("north_america"));
-    var chart_colors = d3.scale.ordinal()
+    let north_america_genre_dimension = ndx.dimension(dc.pluck("genre"));
+    let north_america_genre_sales_group = north_america_genre_dimension.group().reduceSum(dc.pluck("north_america"));
+    let chart_colors = d3.scale.ordinal()
         .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#f1f1f1', '#9800ff', '#ffa500']);
 
+    function north_america_genre_sales_title (k, v) {
+        if (k){
+            return "In the Top 100's, the " + k + " genre has sold " + three_decimal_places(v) + " million games North America.";
+        }
+    }
+
     dc.barChart("#north-america-genre-sales")
-        .width(450)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 75, left: 40})
+        .margins({top: 10, right: 20, bottom: 75, left: 40})
         .dimension(north_america_genre_dimension)
         .group(north_america_genre_sales_group)
         .transitionDelay(500)
@@ -183,30 +210,35 @@ function north_america_genre_sales(ndx) {
             return d.key;
         })
         .colors(chart_colors)
-        .valueAccessor(function(d) {
-            return d.value.toFixed(2)
+        .title(function(d) {
+            return north_america_genre_sales_title(d.key, d.value);
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
+        .useViewBoxResizing(true)
         .xAxisLabel("Genre")
         .yAxisLabel("Games sold (in millions)")
         .yAxis().ticks(8);
-        // .legend(dc.legend().x(550).y(0).itemHeight(15).gap(5));
 }
-// TODO LOOK OVER THE ABOVE CODE TO TRY AND GET LEGEND AND RESIZING WORKING
 
 /* ----- Total Games Sold Per Genre in Europe -----*/
 function europe_genre_sales(ndx) {
-    var europe_genre_dimension = ndx.dimension(dc.pluck("genre"));
-    var europe_genre_sales_group = europe_genre_dimension.group().reduceSum(dc.pluck("europe"));
-    var chart_colors = d3.scale.ordinal()
+    let europe_genre_dimension = ndx.dimension(dc.pluck("genre"));
+    let europe_genre_sales_group = europe_genre_dimension.group().reduceSum(dc.pluck("europe"));
+    let chart_colors = d3.scale.ordinal()
         .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#f1f1f1', '#9800ff', '#ffa500']);
 
+    function europe_genre_sales_title (k, v) {
+        // Shows the game sales per genre in Europe on hover
+        if (k){
+            return "In the Top 100's, the " + k + " genre has sold " + three_decimal_places(v) + " million games in Europe.";
+        }
+    }
+
     dc.barChart("#europe-genre-sales")
-        .width(450)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 75, left: 40})
+        .margins({top: 10, right: 20, bottom: 75, left: 40})
         .dimension(europe_genre_dimension)
         .group(europe_genre_sales_group)
         .transitionDelay(500)
@@ -214,30 +246,35 @@ function europe_genre_sales(ndx) {
             return d.key;
         })
         .colors(chart_colors)
-        .valueAccessor(function(d) {
-            return d.value.toFixed(2)
+        .title(function(d) {
+            return europe_genre_sales_title(d.key, d.value);
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
+        .useViewBoxResizing(true)
         .xAxisLabel("Genre")
         .yAxisLabel("Games sold (in millions)")
         .yAxis().ticks(8);
-    // .legend(dc.legend().x(550).y(0).itemHeight(15).gap(5));
 }
-// TODO LOOK OVER THE ABOVE CODE TO TRY AND GET LEGEND AND RESIZING WORKING
 
 /* ----- Total Games Sold Per Genre in the Rest of World -----*/
 function rest_of_world_genre_sales(ndx) {
-    var rest_of_world_genre_dimension = ndx.dimension(dc.pluck("genre"));
-    var rest_of_world_genre_sales_group = rest_of_world_genre_dimension.group().reduceSum(dc.pluck("rest_of_world"));
-    var chart_colors = d3.scale.ordinal()
+    let rest_of_world_genre_dimension = ndx.dimension(dc.pluck("genre"));
+    let rest_of_world_genre_sales_group = rest_of_world_genre_dimension.group().reduceSum(dc.pluck("rest_of_world"));
+    let chart_colors = d3.scale.ordinal()
         .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#f1f1f1', '#9800ff', '#ffa500']);
 
+    function rest_of_world_genre_sales_title (k, v) {
+        // Shows the game sales per genre in the Rest of the World on hover
+        if (k){
+            return "In the Top 100's, the " + k + " genre has sold " + three_decimal_places(v) + " million games in the Rest of the World.";
+        }
+    }
+
     dc.barChart("#rest-of-world-genre-sales")
-        .width(450)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 75, left: 40})
+        .margins({top: 10, right: 20, bottom: 75, left: 40})
         .dimension(rest_of_world_genre_dimension)
         .group(rest_of_world_genre_sales_group)
         .transitionDelay(500)
@@ -245,80 +282,91 @@ function rest_of_world_genre_sales(ndx) {
             return d.key;
         })
         .colors(chart_colors)
-        .valueAccessor(function(d) {
-            return d.value.toFixed(2)
+        .title(function(d) {
+            return rest_of_world_genre_sales_title(d.key, d.value);
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .elasticY(true)
+        .useViewBoxResizing(true)
         .xAxisLabel("Genre")
         .yAxisLabel("Games sold (in millions)")
-        .yAxis().ticks(8)
-        // .legend(dc.legend().x(550).y(0).itemHeight(15).gap(5));
+        .yAxis().ticks(8);
 }
-// TODO LOOK OVER THE ABOVE CODE TO TRY AND GET LEGEND AND RESIZING WORKING
 
 /*----- Comparison of Genres in Top 100 -----*/
 
 function genre_comparison(ndx) {
-    var genre_comparison_dim = ndx.dimension(dc.pluck("genre"));
-    var genre_comparison_group = genre_comparison_dim.group();
-    var chart_colors = d3.scale.ordinal()
+    let genre_comparison_dim = ndx.dimension(dc.pluck("genre"));
+    let genre_comparison_group = genre_comparison_dim.group();
+    let chart_colors = d3.scale.ordinal()
         .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#f1f1f1', '#9800ff', '#4f9eff', '#ffa500']);
 
+    function genre_comparison_title (k, v) {
+        // Shows the Genre and the number of games that fit the genre in the top 100 on hover
+        if (k){
+            return k + " genre has " + v + " games in the top 100's.";
+        }
+    }
+
     dc.pieChart("#genre-comparison")
-        .height(330)
-        .width(650)
-        .radius(150)
-        .cx(275)
+        .height(250)
+        .radius(85)
+        .cx(85)
         .transitionDuration(1500)
         .dimension(genre_comparison_dim)
         .group(genre_comparison_group)
         .slicesCap(7)
         .renderLabel(false)
+        .title(function(d) {
+            return genre_comparison_title (d.key, d.value);
+        })
         .colorAccessor(function(d) {
             return d.key;
         })
         .colors(chart_colors)
-        .legend(dc.legend().x(450).y(25).gap(3));
-    // TODO LOOK OVER THE BELOW CODE TO TRY AND GET TITLE WORKING
-    // .title(function(d) {
-    //     return console_name_dim + "made" + [global] + "Million total game sales."
-    // });
-    // TODO LOOK OVER THE ABOVE CODE TO TRY AND GET INDIVIDUAL TITLE WORKING
-    // TODO LOOK OVER THE ABOVE CODE TO TRY AND GET INDIVIDUAL TITLE WORKING
+        .legend(dc.legend().x(185).y(25).gap(3));
 }
 
 /*----- Comparison of Publishers in Top 100 -----*/
 
 function publisher_comparison(ndx) {
-    var publisher_comparison_dim = ndx.dimension(dc.pluck("publisher"));
-    var publisher_comparison_group = publisher_comparison_dim.group();
-    var chart_colors = d3.scale.ordinal()
+    let publisher_comparison_dim = ndx.dimension(dc.pluck("publisher"));
+    let publisher_comparison_group = publisher_comparison_dim.group();
+    let chart_colors = d3.scale.ordinal()
         .range(['#003791', '#107c10', '#78727c', '#5dc21e', '#f1f1f1', '#9800ff', '#4f9eff', '#ffa500']);
 
+    function publisher_comparison_title (k, v) {
+        // Shows the Publisher and the number of games the publisher is accountable for on hover
+        if (k) {
+            return k + " has " + v + " games in the top 100's.";
+        }
+    }
+
     dc.pieChart("#publisher-comparison")
-        .height(330)
-        .width(650)
-        .radius(150)
-        .cx(275)
+        .height(250)
+        .radius(85)
+        .cx(85)
         .transitionDuration(1500)
         .dimension(publisher_comparison_dim)
         .group(publisher_comparison_group)
         .slicesCap(7)
         .renderLabel(false)
+        .title(function(d) {
+            return publisher_comparison_title (d.key, d.value);
+        })
         .colorAccessor(function(d) {
             return d.key;
         })
         .colors(chart_colors)
-        .legend(dc.legend().x(450).y(25).gap(3));
-    // TODO LOOK OVER THE BELOW CODE TO TRY AND GET TITLE WORKING
-    // TODO LOOK OVER THE BELOW CODE TO TRY AND GET TITLE WORKING
-    // .title(function(d) {
-    //     return console_name_dim + "made" + [global] + "Million total game sales."
-    // });
-    // TODO LOOK OVER THE ABOVE CODE TO TRY AND GET INDIVIDUAL TITLE WORKING
-    // TODO LOOK OVER THE ABOVE CODE TO TRY AND GET INDIVIDUAL TITLE WORKING
+        .legend(dc.legend().x(185).y(25).gap(3));
+}
+
+function three_decimal_places (v) {
+    // Shows the Publisher and the number of games the publisher is accountable for on hover
+    if (v) {
+        return Number.parseFloat(v).toFixed(3);
+    }
 }
 
 
